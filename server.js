d8a3116ecfc5515b3552fdec79fcdbd9cf8f15d7
Vishnu Kumar <vins.chahar007@gmail.com>
2022-09-22 20:02:45 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    res.json([{}]);
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening at http://%s', process.env.PORT);
+});
